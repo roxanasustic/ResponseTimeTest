98ed7d380a18a8722720fdfa0cb0971f881184b0
Roxana Sustic <61888152+roxanasustic@users.noreply.github.com>
2020-05-09 13:02:20 +0300

    Update GraylogResponseTimeTest.java

--- src/test/java/ResponseTimeTest/GraylogResponseTimeTest.java
+++ src/test/java/ResponseTimeTest/GraylogResponseTimeTest.java
@@ -13,7 +13,7 @@ public class GraylogResponseTimeTest {
     @Test
     public void ResponseTime(){
 
-        RestAssured.baseURI = "http://192.168.99.102:9200/graylog_0/";
+        RestAssured.baseURI = "http://elastic:elastic@192.168.99.102:9200/graylog_0/";
         String json = get("_search?pretty&q=8906d6e1-ef29-4265-9510-b061a7e93127").asString();
         System.out.println(json);
 
